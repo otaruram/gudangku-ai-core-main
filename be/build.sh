508662259826f5f1ae0d09e6ffbe16f3de58d204
otaruram <otaruna61@gmail.com>
2025-12-26 21:07:49 +0700

    Add build.sh and verify schema

---
+++ be/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+echo "Build Start..."
+
+# 1. Install Dependencies
+pip install -r requirements.txt
+
+# 2. Generate Prisma Client (Force download of binaries)
+# We strictly point to the schema to avoid ambiguity
+prisma generate --schema=prisma/schema.prisma
+
+echo "Build Finished Successfully."
